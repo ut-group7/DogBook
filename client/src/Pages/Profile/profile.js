5bb68e10880ba0008035600ce3707f4f21a5dabf
cayce brown <caycebrown98@gmail.com>
2019-08-09 06:15:16 -0500

    cleaned server file and moved authentication routes/logic to routes/api folder

--- client/src/Pages/Profile/profile.js
+++ client/src/Pages/Profile/profile.js
@@ -11,7 +11,7 @@ class Profile extends React.Component {
     };
 
     loadPosts = () => {
-        fetch('http://localhost:3030/auth/testauth', {credentials: 'include', mode: 'cors'})
+        fetch('http://localhost:3030/api/auth/testauth', {credentials: 'include', mode: 'cors'})
         .then(res => res.json())
         .then(res => {res ? console.log(res) : console.log('no res')
             this.setState({data: 'post1', response: res})
