483c478c1596b743ae206d31bc964cd560e3f9ea
fred1n <fred1n@hotmail.com>
2019-08-08 20:54:58 -0500

    added google maps

---
+++ client/src/Pages/Content/Gmap.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as lostDogs from "../../Data/lostdogs.json"
+// import dgicon from "../Content/mapdog.svg";
+
+
+
+function Map() {
+  const [selectedDog, setselectedDog] = useState(null);
+
+  return (
+  <GoogleMap
+    defaultZoom={10}
+    // defaults to Austin location
+    defaultCenter={{ lat: 30.267153, lng: -97.7430608 }}
+  >
+      {lostDogs.features.map(dog => (
+        <Marker 
+          key={dog.properties.DOG_ID} 
+          position={{
+            lat: dog.geometry.coordinates[0],
+            lng: dog.geometry.coordinates[1]
+        }}
+        onClick={() => {
+          setselectedDog(dog);
+        }}
+        // icon={{
+        //   url:{ dgicon },
+        //   // url: "../Content/dog.svg",
+        //   scaledSize: new window.google.maps.Size(25, 25)
+        // }}
+
+        />
+        ))}
+         {selectedDog && (
+          <InfoWindow 
+            position={{
+              lat: selectedDog.geometry.coordinates[0],
+              lng: selectedDog.geometry.coordinates[1],
+            }}
+            onCloseClick={() => {
+              setselectedDog(null);
+            }}
+           >
+          <div>
+            <h2>{selectedDog.properties.NAME}</h2>
+            <p>{selectedDog.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+            )}
+    </GoogleMap>
+  );
+}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div className="Gmap" style={{ width: "100%", height: "300px" }}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?&key=${
+          "AIzaSyCbt-z1JLyXUZwff9iG3C70Ic6tHdJYv3o"
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
--- client/src/Pages/Main/Main.js
+++ client/src/Pages/Main/Main.js
@@ -1,17 +1,56 @@
 import React from "react";
-import imageUrl from '../Content/background.jpg';
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Map from '../Content/Gmap';
 import './Main.css';
+import Pic1 from '../Content/pic1.png';
+import Pic2 from '../Content/pic2.png';
+import Pic3 from '../Content/pic3.png';
+import Pic4 from '../Content/pic4.png';
 
-function Main () {
-    return (
-        <div className='Main-page' style={{backgroundImage: `url(${imageUrl})` }}>
-            <div className="App-content">
-                <h1>Recent listings go here</h1>
-            
-                <h2>A map will go here</h2>
-            </div>
-        </div>
-    )
+
+
+const Main = () => {
+  return (
+    <MDBContainer className="mt-5 text-left">
+      <MDBRow>
+        <MDBCol>
+          <MDBJumbotron>
+            <h2 className="h1 display-3">Canine Connections</h2>
+            <p className="mapinfo">
+                Bringing and connecting lost pets back with their families. Austin map showing locations of lost pets.
+            </p>
+            <a className="map">
+                <Map />
+            </a>
+            <hr className="my-2" />
+            <h2 className="h1 display-3 text-center">Lost Pets</h2>
+            <p>
+             Have you seen any of the pets? If you have, please click on the picture to provide information where the pet was last seen. 
+            </p>
+
+            <MDBRow>
+            <MDBCol>
+                <p className="view overlay my-4">
+                <img src={Pic1} alt="pics" style={{ width: "300px", height: "300px" }}/>
+                </p>
+            </MDBCol>
+            <MDBCol>
+                <p className="view overlay my-4">
+                <img src={Pic2} alt="pics" style={{ width: "300px", height: "300px" }}/>
+                </p>
+            </MDBCol>
+            <MDBCol>
+                <p className="view overlay my-4">
+                <img src={Pic3} alt="pics"style={{ width: "300px", height: "300px" }}/>
+                </p>
+            </MDBCol>
+         </MDBRow>
+        </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  )
 }
 
 export default Main;
