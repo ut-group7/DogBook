c556b210d7cb215cc0b746f7d0f35a5e27830aba
cayce brown <caycebrown98@gmail.com>
2019-08-16 18:26:16 -0500

    added google button image and made some minor style changes

--- client/src/Components/LoginBox/LoginBox.js
+++ client/src/Components/LoginBox/LoginBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from '../LoginButton/login';
+import GoogleButton from '../../assets/btn_google_signin_dark_normal_web.png';
 require('./loginBox.css');
 
 const LoginBox = () => {
@@ -11,8 +12,11 @@ const LoginBox = () => {
     return (
         <div className="container-fluid loginWrapper">
             <div className="col loginBox">
-                <h1>Welcome, Login with Google!</h1>
+                <h1>Welcome,</h1>
+                <h3> Sign in with Google below to make a post</h3>
+                <div className="divider"></div>
                 <Login></Login>
+                <a><img src={GoogleButton}></img></a>
             </div>
         </div>
 
