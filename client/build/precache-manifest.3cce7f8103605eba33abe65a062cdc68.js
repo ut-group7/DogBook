fa30fa7c066a57371bfdf3eb60e54bcea46c45ef
Gregory Rutherford <gregorford@gmail.com>
2019-08-16 11:39:50 -0500

    fixed git ignore file

---
+++ client/build/precache-manifest.3cce7f8103605eba33abe65a062cdc68.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eeb663c7f423352dcd5ff661f3fbc8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb971d424b0f4cff9c68",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "96ee74e4d9427dd102e6",
+    "url": "/static/css/main.70d801d1.chunk.css"
+  },
+  {
+    "revision": "cb971d424b0f4cff9c68",
+    "url": "/static/js/2.837b2f2a.chunk.js"
+  },
+  {
+    "revision": "96ee74e4d9427dd102e6",
+    "url": "/static/js/main.13f7b76d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a933e6f874c698f33bdf62410866018",
+    "url": "/static/media/logo.0a933e6f.png"
+  },
+  {
+    "revision": "378954a4d3c71dc8e645657aa82854e9",
+    "url": "/static/media/lost-dog.378954a4.jpg"
+  },
+  {
+    "revision": "c194247d9199a47533a1d850a20ae15e",
+    "url": "/static/media/noise_pattern_with_crosslines.c194247d.png"
+  },
+  {
+    "revision": "ac10e1ef525b594b447d025ef3266012",
+    "url": "/static/media/pic1.ac10e1ef.png"
+  },
+  {
+    "revision": "8adb191e58366ab7858d77036e1d340a",
+    "url": "/static/media/pic2.8adb191e.png"
+  },
+  {
+    "revision": "e36c5be9d5f4411746156d19f8ef1196",
+    "url": "/static/media/pic3.e36c5be9.png"
+  },
+  {
+    "revision": "0b1ab55cdfcc9afa72633b2e8482df7f",
+    "url": "/static/media/pic4.0b1ab55c.png"
+  },
+  {
+    "revision": "b9f4e5a3aa6f65f95f2a9716b0f8cf67",
+    "url": "/static/media/seen-dog.b9f4e5a3.png"
+  }
+]);
