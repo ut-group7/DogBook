20a4267076a09140936cb0eef112966cc89b3bc5
Gregory Rutherford <gregorford@gmail.com>
2019-08-16 11:11:11 -0500

    fixed server code for serving static file

--- server.js
+++ server.js
@@ -16,15 +16,24 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.errorHandler());
 app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 
 app.configure(express.rest());
 app.configure(socketio());
