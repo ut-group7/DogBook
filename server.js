9ac15d2029d01ee20059c0ba83fabafe8d6d2770
Gregory Rutherford <gregorford@gmail.com>
2019-08-16 11:05:37 -0500

    not the build scripts, server code wrong

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.errorHandler());
 app.use(routes);
 
-app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.configure(express.rest());
 app.configure(socketio());
