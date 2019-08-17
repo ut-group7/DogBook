af3af932a4042283eeefba49c2097091060bca5c
cayce brown <caycebrown98@gmail.com>
2019-08-16 22:56:07 -0500

    updated Gmap logic to make markers and windows after pulling drom DB, created InfoCard component

---
+++ client/src/Components/InfoCard/InfoCard.js
@@ -0,0 +1,63 @@
+import  React, { useState, useEffect } from 'react';
+import  Card  from 'react-bootstrap/Card';
+
+const InfoCard = function() {
+    const [data, setData] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
+
+    const fetchData = async () => {
+      setisLoading(true);
+      const response = await fetch("http://localhost:3030/api/lostDogs");
+      const json = await response.json();
+      setData(json);
+      setisLoading(false);
+    };
+  
+    useEffect(() => {
+      fetchData(data);
+    }, []);
+
+
+
+    const rowStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        xOverflow: 'scroll',
+    };
+
+    if (isLoading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+
+    return (
+    <div style={rowStyle}>
+    {data.map(post => {
+        return (
+            <Card style={{ width: '18rem', marginLeft: '2rem', marginRight: '2rem', display: 'flex' }}>
+                <Card.Body>
+                    <Card.Img variant='top' style={{ height: '200px', width: '200px' }} src={post.img}></Card.Img>
+                    <Card.Title>Breed: {post.dogBreed}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Color: {post.dogColor}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Size: {post.dogSize}</Card.Subtitle>
+                    <Card.Text>
+                        Notes: {post.notes}
+                    </Card.Text>
+                    <Card.Text>
+                        Reward: {post.reward}
+                    </Card.Text>
+                    <Card.Text>
+                        Contact: {post.email}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        )})}
+    </div>
+    
+    )
+} 
+
+export default InfoCard;
--- client/src/Pages/Content/Gmap.js
+++ client/src/Pages/Content/Gmap.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
 import * as lostDogs from "../../Data/lostdogs.json"
@@ -8,6 +8,21 @@ const GOOGLE_MAP_KEY = "AIzaSyCx5H0EDQpmccKnYt_cZt3Qifvy0n3yD6I";
 
 function Map() {
   const [selectedDog, setselectedDog] = useState(null);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:3030/api/lostDogs");
+    const json = await response.json();
+    setData(json);
+  };
+
+  useEffect(() => {
+    fetchData(data);
+  }, []);
+
+  const imgStyle = {
+    height: '160px',
+  }
 
   return (
   <GoogleMap
@@ -15,37 +30,36 @@ function Map() {
     // defaults to Austin location
     defaultCenter={{ lat: 30.267153, lng: -97.7430608 }}
   >
-      {lostDogs.features.map(dog => (
+      {data.map(dog => (
+        
         <Marker 
-          key={dog.properties.DOG_ID} 
+          key={dog.location} 
           position={{
-            lat: dog.geometry.coordinates[0],
-            lng: dog.geometry.coordinates[1]
+            lat: +(dog.location.lat),
+            lng: +(dog.location.lng)
         }}
         onClick={() => {
           setselectedDog(dog);
         }}
-        // icon={{
-        //   url:{ dgicon },
-        //   // url: "../Content/dog.svg",
-        //   scaledSize: new window.google.maps.Size(25, 25)
-        // }}
+
 
         />
         ))}
+
          {selectedDog && (
           <InfoWindow 
             position={{
-              lat: selectedDog.geometry.coordinates[0],
-              lng: selectedDog.geometry.coordinates[1],
+              lat: selectedDog.location.lat,
+              lng: selectedDog.location.lng,
             }}
             onCloseClick={() => {
               setselectedDog(null);
             }}
            >
           <div>
-            <h2>{selectedDog.properties.NAME}</h2>
-            <p>{selectedDog.properties.DESCRIPTIO}</p>
+            <h4>Breed: {selectedDog.dogBreed}</h4>
+            <p>Size: {selectedDog.dogSize}</p>
+            <img src={selectedDog.img} style={imgStyle}></img>
           </div>
         </InfoWindow>
             )}
@@ -56,7 +70,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
   return (
-    <div className="Gmap" style={{ width: "100%", height: "300px" }}>
+    <div className="Gmap" style={{ width: "100%", height: "450px" }}>
       <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?&key=${
           GOOGLE_MAP_KEY }`}
