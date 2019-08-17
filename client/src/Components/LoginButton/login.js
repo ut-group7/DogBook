3e50e04be4026e1c12829017df54d24b802d8402
Gregory Rutherford <gregorford@gmail.com>
2019-08-16 23:00:28 -0500

    fixed href on loginbox component

--- client/src/Components/LoginButton/login.js
+++ client/src/Components/LoginButton/login.js
@@ -6,7 +6,7 @@ const Login = () => {
         
     return(
         <div>
-            <a className="google" href='http://localhost:3030/api/auth/google'><img src={GoogleButton}></img></a>  
+            <a className="google" href='/api/auth/google'><img src={GoogleButton}></img></a>  
         </div>
     )
 }
